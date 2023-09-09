0d0630d3afe4a0dfdb742efde6c2a7ff1952f73a
TOOBCHAT <toobai.snehashis@gmail.com>
2023-09-09 11:15:36 +0530

    leapYear program

---
+++ leapYear.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int year;
+printf("enter the year: ");
+scanf("%d", &year);
+(year%4==0 && year%100!=0)?
+printf("leap year"): (year%400==0)?
+printf("leap year"): printf("not a leap year");
+return 0;
+}
